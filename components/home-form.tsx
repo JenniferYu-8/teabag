2cd7e693062db821c580736ff0b487d5658e2ca4
Jennifer-Yu8 <jenniferyu.on@gmail.com>
2025-01-18 17:11:50 -0500

    navigate to results page when submitted

--- components/home-form.tsx
+++ components/home-form.tsx
@@ -1,27 +1,27 @@
 "use client";
 
-import React, { useState } from "react";
-import { ReactMic } from "react-mic";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { getFirestore, doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
+import app from "../firebase/clientApp"; // your Firebase config
 import Image from 'next/image';
-import {useCollection} from "react-firebase-hooks/firestore"
-import app from "../firebase/clientApp";
-import firebase from "firebase/compat/app";
-import { getFirestore, addDoc, doc, setDoc, getDoc, updateDoc, collection, Firestore, QuerySnapshot, DocumentData } from "firebase/firestore";
+import { ReactMic } from "react-mic";
 import { BsFillTrashFill } from "react-icons/bs";
 import { BsFillStopCircleFill } from "react-icons/bs";
 import { BsFillRecordFill } from "react-icons/bs";
-
-
-require('dotenv').config();
-
+import Link from "next/link";
 
 export default function HomeForm() {
   const [name, setName] = useState("");
   const [secondDropdown, setSecondDropdown] = useState("");
   const [isRecording, setIsRecording] = useState(false);
   const [audioURL, setAudioURL] = useState("");
+  const [useTextarea, setUseTextarea] = useState(false);
   const [textareaValue, setTextareaValue] = useState("");
-  const [sessionsForUser, setSessionsForUser] = useState<string[]>([]); ;
+  const [sessionsForUser, setSessionsForUser] = useState<string[]>([]);
+  const router = useRouter(); // Ensure you call it inside a page component
+
+  const db = getFirestore(app);
 
   const handleStartRecording = () => setIsRecording(true);
   const handleStopRecording = (recordedBlob: { blob: Blob | MediaSource; }) => {
@@ -29,84 +29,54 @@ export default function HomeForm() {
     setAudioURL(URL.createObjectURL(recordedBlob.blob));
   };
 
-  //backend starts here
-  const db = getFirestore(app);
-  const yapsCollectionRef = collection(db, "yaps");
-  // const [yapData, yapLoading, yapError] = useCollection(yapsCollectionRef, {});
-  // const user = "wooooo";
-    
-  // if (!yapLoading && yapData) {
-  //   yapData.docs.map((doc) => console.log(doc.data()));
-  // }
-
-  //add feature
   const onSubmit = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault(); // remove later?
-    console.log(textareaValue);
-    console.log(audioURL);
+    e.preventDefault(); // Prevent form submission and page reload
 
-    const docRef = doc(db, "yaps", name.toLowerCase());
-    const docSnapshot = await getDoc(docRef);
-
-    // Validate the name and ensure it is not empty
-    const trimmedName = name.trim();
-    if (!trimmedName) {
+    if (!name.trim()) {
       alert("Please enter a name.");
       return;
     }
 
-    // Validate that either textareaValue or audioURL has content
     if (!textareaValue.trim() && !audioURL) {
       alert("Please record audio or enter text.");
       return;
     }
 
+    const docRef = doc(db, "yaps", name.toLowerCase());
+
+    const docSnapshot = await getDoc(docRef);
+
     if (docSnapshot.exists()) {
-      // Access the document ID
       const data = docSnapshot.data();
       const sessionCount = Object.keys(data).filter(key => key.startsWith('Session')).length;
-      if (secondDropdown !== "Nope, this is a new yap session") {
-        console.log("WORKED!")
-        const docRef2 = doc(db, "yaps", name.toLowerCase());
-        try {
-          await updateDoc(docRef2, {
-            [`${secondDropdown}.yap`]: textareaValue ? textareaValue : audioURL, // textareaValue takes precedence
-
-          }); 
-        } catch (error) {
-          console.error("Error updating document: ", error);
-        }
-      }
-      else {
-        try {
-          await updateDoc(docRef, {
-            [`Session ${String(sessionCount + 1)}`]: {
-              name,
-              secondDropdown,
-              yap: textareaValue ? textareaValue : audioURL,
-            }
-          });  
-        } catch (error) {
-          console.error("Error updating session count: ", error);
-        }
-      }
-    } else {
-      try {
-        await setDoc(docRef, {
-        "Session 1": {
+      const sessionField = `Session ${sessionCount + 1}`;
+      await updateDoc(docRef, {
+        [sessionField]: {
           name,
           secondDropdown,
           yap: textareaValue ? textareaValue : audioURL,
-        },
+        }
+      });
+    } else {
+      try {
+        await setDoc(docRef, {
+          "Session 1": {
+            name,
+            secondDropdown,
+            yap: textareaValue ? textareaValue : audioURL,
+          }
         });
         console.log("Document written with custom ID: ", docRef.id);
       } catch (error) {
         console.error("Error adding document: ", error);
       }
-    } 
-  }
+    }
 
-  //add onChangeName feature
+    // After successful submission, redirect to results page
+    router.push("/results");
+  };
+
+    //add onChangeName feature
   const onChangeName = async (newName: string) => {
     console.log("we are running");
     const docRef = doc(db, "yaps", newName.toLowerCase());
@@ -122,18 +92,18 @@ export default function HomeForm() {
       setSessionsForUser([]); 
     }
   };
-    
+
   return (
     <section>
       <div className="flex flex-col items-center justify-center">
-        <div>
-          <Image src="/teabag.png" alt="Teabag logo" width="192" height="192" quality="95"></Image>
-        </div>
+         <div>
+           <Image src="/teabag.png" alt="Teabag logo" width="192" height="192" quality="95"></Image>
+         </div>
 
-        <form className="mt-3 flex flex-col gap-3">
-          <label className="flex flex-col">
-            Yapper's name
-            <input
+        <form onSubmit={onSubmit} className="mt-5 flex flex-col gap-3">
+           <label className="flex flex-col">
+             Yapper's name
+             <input
               type="text"
               value={name}
               onChange={(e) => {
@@ -229,19 +199,275 @@ export default function HomeForm() {
               ></textarea>
             </div>
           </div>
-
-        {/* Submit Button */}
-        <button
-          type="submit"
-          className="px-4 py-2 bg-[#C2D02F] text-black rounded hover:bg-[#AFBC29] border border-gray-900"
-          onClick={onSubmit}
-        >
-          Submit
-        </button>
-
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-[#C2D02F] text-black rounded hover:bg-[#AFBC29] border border-gray-900"
+          >
+            Submit
+          </button>
         </form>
       </div>
-
     </section>
   );
 }
+
+
+
+
+// "use client";
+
+// import React, { useState } from "react";
+// import { ReactMic } from "react-mic";
+// import Image from 'next/image';
+// import {useCollection} from "react-firebase-hooks/firestore"
+// import app from "../firebase/clientApp";
+// import firebase from "firebase/compat/app";
+// import { getFirestore, addDoc, doc, setDoc, getDoc, updateDoc, collection, Firestore, QuerySnapshot, DocumentData } from "firebase/firestore";
+// import { BsFillTrashFill } from "react-icons/bs";
+// import { BsFillStopCircleFill } from "react-icons/bs";
+// import { BsFillRecordFill } from "react-icons/bs";
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+
+
+// require('dotenv').config();
+
+
+// export default function HomeForm() {
+//   const [name, setName] = useState("");
+//   const [secondDropdown, setSecondDropdown] = useState("");
+//   const [isRecording, setIsRecording] = useState(false);
+//   const [audioURL, setAudioURL] = useState("");
+//   const [textareaValue, setTextareaValue] = useState("");
+//   const [sessionsForUser, setSessionsForUser] = useState<string[]>([]); ;
+
+//   const handleStartRecording = () => setIsRecording(true);
+//   const handleStopRecording = (recordedBlob: { blob: Blob | MediaSource; }) => {
+//     setIsRecording(false);
+//     setAudioURL(URL.createObjectURL(recordedBlob.blob));
+//   };
+
+//   //backend starts here
+//   const db = getFirestore(app);
+//   const yapsCollectionRef = collection(db, "yaps");
+
+//   // Inside your component
+//   const router = useRouter();
+//   // const [yapData, yapLoading, yapError] = useCollection(yapsCollectionRef, {});
+//   // const user = "wooooo";
+    
+//   // if (!yapLoading && yapData) {
+//   //   yapData.docs.map((doc) => console.log(doc.data()));
+//   // }
+
+//   //add feature
+//   const onSubmit = async (e: { preventDefault: () => void; }) => {
+//     e.preventDefault(); // remove later?
+//     console.log(textareaValue);
+//     console.log(audioURL);
+
+//     const docRef = doc(db, "yaps", name.toLowerCase());
+//     const docSnapshot = await getDoc(docRef);
+
+//     // Validate the name and ensure it is not empty
+//     const trimmedName = name.trim();
+//     if (!trimmedName) {
+//       alert("Please enter a name.");
+//       return;
+//     }
+
+//     // Validate that either textareaValue or audioURL has content
+//     if (!textareaValue.trim() && !audioURL) {
+//       alert("Please record audio or enter text.");
+//       return;
+//     }
+
+//     if (docSnapshot.exists()) {
+//       // Access the document ID
+//       const data = docSnapshot.data();
+//       const sessionCount = Object.keys(data).filter(key => key.startsWith('Session')).length;
+//       if (secondDropdown !== "Nope, this is a new yap session") {
+//         console.log("WORKED!")
+//         const docRef2 = doc(db, "yaps", name.toLowerCase());
+//         try {
+//           await updateDoc(docRef2, {
+//             [`${secondDropdown}.yap`]: textareaValue ? textareaValue : audioURL, // textareaValue takes precedence
+
+//           }); 
+//         } catch (error) {
+//           console.error("Error updating document: ", error);
+//         }
+//       }
+//       else {
+//         try {
+//           await updateDoc(docRef, {
+//             [`Session ${String(sessionCount + 1)}`]: {
+//               name,
+//               secondDropdown,
+//               yap: textareaValue ? textareaValue : audioURL,
+//             }
+//           });  
+//         } catch (error) {
+//           console.error("Error updating session count: ", error);
+//         }
+//       }
+//     } else {
+//       try {
+//         await setDoc(docRef, {
+//         "Session 1": {
+//           name,
+//           secondDropdown,
+//           yap: textareaValue ? textareaValue : audioURL,
+//         },
+//         });
+//         console.log("Document written with custom ID: ", docRef.id);
+//       } catch (error) {
+//         console.error("Error adding document: ", error);
+//       }
+//     } 
+
+//     router.push("/results");
+//   }
+
+//   //add onChangeName feature
+//   const onChangeName = async (newName: string) => {
+//     console.log("we are running");
+//     const docRef = doc(db, "yaps", newName.toLowerCase());
+//     const docSnapshot = await getDoc(docRef);
+//     if (docSnapshot.exists()) {
+//       // Access the document data
+//       const data = docSnapshot.data();
+//       let sessionCount = Object.keys(data);
+//       setSessionsForUser(sessionCount); 
+//       console.log(sessionCount);  
+//     }
+//     else {
+//       setSessionsForUser([]); 
+//     }
+//   };
+    
+//   return (
+//     <section>
+//       <div className="flex flex-col items-center justify-center">
+//         <div>
+//           <Image src="/teabag.png" alt="Teabag logo" width="192" height="192" quality="95"></Image>
+//         </div>
+
+//         <form className="mt-3 flex flex-col gap-3">
+//           <label className="flex flex-col">
+//             Yapper's name
+//             <input
+//               type="text"
+//               value={name}
+//               onChange={(e) => {
+//                 const newName = e.target.value;
+//                 setName(newName);
+//                 onChangeName(newName); // Pass the new name to the function
+//               }}
+//               required
+//               className="p-2.5 rounded-full bg-gray-100 focus:bg-gray-50"
+//             />
+//           </label>
+//           <br />
+
+//           {/* Dropdown 2 */}
+//           <label className="flex flex-col">
+//             Do you want to add onto a certain conversation?:
+//             <select
+//               value={secondDropdown}
+//               onChange={(e) => setSecondDropdown(e.target.value)}
+//               required
+//               className="p-2.5 bg-gray-100 focus:bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full"
+//             >
+//               <option value="" disabled>
+//                 Select...
+//               </option>
+              
+//               <option value="new-yap">Nope, this is a new yap session</option>
+//               {sessionsForUser.map((user) => (
+//                   <option key={user} value={user}>{user}</option>
+//               ))} 
+//               </select>
+//           </label>
+//           <br />
+
+//           <div className="flex flex-col md:flex-row gap-8">
+//             {/* Audio Recorder */}
+//             <div className="flex flex-col items-start">
+//               <h3>Audio Recorder</h3>
+//               <ReactMic
+//                 record={isRecording}
+//                 onStop={handleStopRecording}
+//                 mimeType="audio/webm"
+//                 className="w-[160] h-12 rounded-md"
+//               />
+//               <div className="flex gap-16 mt-2 w-100">
+//                 <button
+//                   type="button"
+//                   onClick={handleStartRecording}
+//                   disabled={isRecording}
+//                   className="px-4 py-2 bg-[#C2D02F] text-black rounded hover:bg-[#AFBC29] disabled:bg-gray-300"
+//                 >
+//                  <BsFillRecordFill />
+//                 </button>
+//                 <button
+//                   type="button"
+//                   onClick={() => setIsRecording(false)}
+//                   disabled={!isRecording}
+//                   className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 disabled:bg-gray-300"
+//                 >
+//                   <BsFillStopCircleFill />
+//                 </button>
+//               </div>
+//               {audioURL && (
+//                 <div className="mt-4">
+//                   <h4>Recorded Audio:</h4>
+//                   <audio controls src={audioURL}
+//                   className="w-[160]"
+//                   ></audio>
+//                   <div className="flex gap-2 mt-2">
+//                     <button
+//                       type="button"
+//                       onClick={() => setAudioURL("")}
+//                       className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
+//                     >
+//                       <BsFillTrashFill />
+//                     </button>
+//                   </div>
+//                 </div>
+//               )}
+//             </div>
+
+//             <div>
+//               <p className="py-[50]">OR</p>
+//             </div>
+
+//             {/* Textarea */}
+//             <div className="flex flex-col items-start">
+//               <h3>Enter Text</h3>
+//               <textarea
+//                 value={textareaValue}
+//                 onChange={(e) => setTextareaValue(e.target.value)}
+//                 className="w-full h-full p-2 border border-gray-300 rounded-md"
+//               ></textarea>
+//             </div>
+//           </div>
+
+//         {/* Submit Button */}
+//         <Link href={`results`}>
+//           <button
+//             type="submit"
+//             className="w-full px-4 py-2 bg-[#C2D02F] text-black rounded hover:bg-[#AFBC29] border border-gray-900"
+//             onClick={onSubmit}
+//           >
+//             Submit
+//           </button>
+//         </Link>
+
+
+//         </form>
+//       </div>
+
+//     </section>
+//   );
+// }
--- langchain_helper.py
+++ langchain_helper.py
@@ -45,3 +45,66 @@ if __name__ == "__main__":
     organized_story = reorganize_story(unorganized_story)
     print("Reorganized Story:\n")
     print(organized_story)
+
+    #1104 words
+    sarah = """
+    Guys I woke up from a very scary dream. 
+    
+    Elaine you were in it but like essentially it was set in a very dark English town where like I went on as a trip or something and Elaine’s family discovered a secret government document in the cabin they rented but they knew if anyone especially the police if the police knew or if they made any big moves the aliens would catch a signal and both government and aliens would come for this location to get the map. 
+    
+    So Elaine family put the map back where they found it but Elaine and Timmy do not know why the map needs to be hidden only her parents and I know. 
+    
+    And then we explored the English town I. The evening it was a very abandoned seaside town it was very ominous very lonely but somehow a lot of people in the town but it wasn’t lively despite the crowded streets. 
+    
+    It felt like the people around us were almost like ghosts yk.Elaine kept on going on about some fish biting and I went with her to find fish on a very tall bridge.
+
+    Us two ended up in a very remote area far from town on a cliffside by the sea it was very dark and late In the evening the wind was like super big and the waves were like scary scary as well.
+
+    Elaine wasn’t scared for some reason and was like Sarah hurry up we have to find the fish that can bite off fingers I want to see it.
+
+    We ended up wandering into the woods next a very dark misty woods where we found a cabin.
+
+    We went in it was an old fisherman small single room cabin in the middle of a very ominous dark forest and Elaine freaking went right in when I was super scared.
+
+    in there there was a lot of equipment for fishing and a desk with a chest on there and Elaine opened the chest and found a map while I was distracted by the fishing equipment and a journal that I didn’t even open but somehow just looking at the journal it told me everything about why I can’t let anyone know we have “THE map” and that the aliens are always watching to we can’t have our emotions change too drastically otherwise they would catch on and locate us which by then we would not be able to escape.
+
+    The journal gave me insights to the future the thing is whenever I wanted to tell someone about it to prevent the future from happening I couldn’t and I physically couldn’t change the future either my body wouldn’t move.
+
+    Elaine then found THE map while I was like frozen in the same spot terrified at the future I caught a glimpse of and she naively said omg this map is so cool it looks like the one the police were looking for when we went into the police station the other day! Let’s return it to them!
+
+    Elaine also said she needed to send a note on this ominous website about a cool thing that happened recently because everyone is doing it it is a global trend and the note will be uploaded at the same time as everyone else’s which is at  exactly 12:00 am.
+
+    What everyone didn’t know is I knew the website was just the scheme of I shouldn’t even call them aliens but they were powerful shapeless you sense their presence and it’s very suffocating but entities.
+
+    The entities created that site to track who found that map and once notified you have 10 minutes before they arrive.
+
+    I wanted to stop Elaine but I couldn’t and my body physically prevented me as my body or my dream self encouraged Elaine enthusiastically I was really panicking in like a dark trapped space trying to break out and tell Elaine to stop.
+
+    we went to the police station afterwards I was still resisting but like how do I explain it but I was so stressed the entire time because i couldn’t control myself like that was the most terrifying thing.
+
+    Elaine casually gave the map to the police the police was like what a weird girl giving us a piece of paper it was rolled up btw so the police didn’t realize immediately what it was at first.
+
+    After Elaine left I saw the police realize it was the map and they all started panicking and calling up their chain of command the fbi and the people behind the fbi and all of the police were in action I watched all of this in third person.
+
+    All of a sudden I regained control of my body but by then we had only 10 minutes and our cabins with our families were on the other side of town and I didn’t even have a chance to explain I just grabbed Elaine and started running because I knew if I didn’t they would take her I was actually so scared I would lose my pookie Elaine xia 🥹🥹🥹🥹.
+
+    And the entire time I could feel everything I could feel the pressure I can sense the entities coming closer it was the very suffocating feeling that was so terrifying but all I could do was run and try to hide Elaine as fast as I can.
+
+    I brought her back to the cabin in the woods whichbdsomehow appeared on our way back to the cabin with our families and my instinct told me we have to hide here instead.
+
+    I tucked Elaine away and I hid in the closet myself but then I heard Elaine repeatedly saying numbers numbers like it was terrifying because what was Elaine saying I couldn’t tell was it coordinated was it code was it what was it I was so panicked and I realized I was too scared to tell her to be quiet.
+
+    At the last moment I realized she was counting down it wasn’t Morse it wasn’t coordinated it was a countdown.
+
+    And I felt the pressure heighten as I realized it was a countdown, the countdown for when the entities would arrive and I couldn’t do anything to break the timer.
+
+    And then my alarm went off.
+
+    whoever I was so scared of they were at the door of the cabin when my alarm went off.
+
+    This was actually and probably one of my scariest dreams yet 😭😭😭.
+
+    It just felt so tense because everything felt like it was timed in the first place and the countdown really like it was so stressful.
+
+
+"""
