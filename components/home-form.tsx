ae2026804938c3042a0038528744f78210a54ae7
Jennifer-Yu8 <jenniferyu.on@gmail.com>
2025-01-18 14:17:20 -0500

    edit tailwind config file

--- components/home-form.tsx
+++ components/home-form.tsx
@@ -86,14 +86,13 @@ export default function HomeForm() {
         <form onSubmit={handleSubmit} className="mt-5 flex flex-col gap-3">
           {/* Dropdown 1 */}
           <label className="flex flex-col">
-            Enter your name:
+            Yapper's name
             <input
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter your name"
+              className="h-14"
             />
           </label>
           <br />
