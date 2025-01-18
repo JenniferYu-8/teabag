d58e96d6e29903d91aaaca6339de26fc1e36b435
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2025-01-18 12:27:58 -0500

    firebase database  implemented

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -3,18 +3,24 @@ import "firebase/auth";
 import "firebase/firestore"; 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const clientCredentials =  {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
 
-const app = initializeApp(clientCredentials);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-export default app; 
+export default app;
